111ee534c340e5ca55377a89cc63d94098309803
ÖMER FARUK YILMAZ <omerfaruk2441@gmail.com>
2024-10-03 23:35:50 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
 
         /*
         Set<Task> annsTasks = new HashSet<>();
